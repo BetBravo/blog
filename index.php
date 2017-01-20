6d673adf8e611be6b02b2cbcdddcae3486a59c33
Betsabe Bravo <alexsabeth.b@gmail.com>
2017-01-20 10:57:43 -0400

    mc juntos

--- index.php
+++ index.php
@@ -3,26 +3,16 @@ $link = mysql_connect('localhost', 'root', '123456');
 mysql_select_db('blog_db', $link);
 
 $result = mysql_query('SELECT id, title FROM post', $link);
-var_dump(mysql_fetch_assoc($result)); die();
-?>
 
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = mysql_fetch_assoc($result)): ?>
-                <li>
-                    <a href="/show.php?id=<?php echo $row['id'] ?>">
-                        <?php echo $row['title'] ?>
-                    </a>
-                </li>
-            <?php endwhile; ?>
-        </ul>
-    </body>
-</html>
+$posts = array();
 
-<?php
+while ($row = mysql_fetch_assoc($result)) {
+    $posts[] = $row;
+}
+
+//var_dump($posts);die();
+ 
 mysql_close($link);
+ 
+// incluye el código HTML de la vista
+require 'templates/list.php';
